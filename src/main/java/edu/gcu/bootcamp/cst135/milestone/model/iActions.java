68a81f496d4bb0931640a8dce3322df8c874453c
Roy Chancellor <roychance600@gmail.com>
2019-06-29 23:04:55 -0700

    Added DEPOSIT and WITHDRAWAL constants to iAction
    Created constants DEPOSIT = 1 and WITHDRAWAL = -1 to be descriptors
    passed to processTransaction to be used as multipliers in the
    transaction calculation

--- src/main/java/edu/gcu/bootcamp/cst135/milestone/model/iActions.java
+++ src/main/java/edu/gcu/bootcamp/cst135/milestone/model/iActions.java
@@ -2,6 +2,13 @@ package edu.gcu.bootcamp.cst135.milestone.model;
 
 public interface iActions {
 	/**
+	 * DEPOSIT and WITHDRAW are provided as descriptors for transType in processTransaction
+	 * and can be used as multipliers depending on the transaction type
+	 */
+	public static final int DEPOSIT = 1;
+	public static final int WITHDRAWAL = -1;
+	
+	/**
 	 * interface method for getting a double value from a Scanner
 	 * @param message
 	 * @return
